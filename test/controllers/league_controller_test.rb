c78a187a219a5a04337232c113bbf76a30419660
Pedro Mateus <pedropmateus91@gmail.com>
2026-06-23 17:03:31 +0100

    Fix league_controller_test.rb

--- test/controllers/league_controller_test.rb
+++ test/controllers/league_controller_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class LeagueControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @season = Season.create!(name: "Season 1", year: 2024, active: true)
+  end
+
   test "should get index" do
-    get league_index_url
+    get root_path
     assert_response :success
   end
 end
